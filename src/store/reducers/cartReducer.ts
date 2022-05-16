078f5f18e2f4f9bffc88d3fb01c2ae4c1c269a18
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-16 18:32:55 +0300

    Fixed eslint errors in cartReducer

--- src/store/reducers/cartReducer.ts
+++ src/store/reducers/cartReducer.ts
@@ -1,13 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { Product } from '../../models/ProductModel';
 
-const findById = (state : {value: Product[]}, action: {payload: Product}) => (
-  state.value.find((n) => n.id === action.payload.id)
-);
-const findByIdNumber = (state : {value: Product[]}, action: {payload: number}) => (
-  state.value.find((n) => n.id === action.payload)
-);
-
 export const cartSlice = createSlice({
   name: 'cart',
   initialState: {
@@ -15,22 +8,27 @@ export const cartSlice = createSlice({
   },
   reducers: {
     add: (state: {value: Product[]}, action: {payload: Product}) => {
-      if (findById(state, action)) {
-        // @ts-ignore
-        findById(state, action).count += action.payload.count;
-      } else state.value = [...state.value, action.payload];
+      const thisProduct = state.value.find((n) => n.id === action.payload.id);
+      if (thisProduct) {
+        if (thisProduct) {
+          thisProduct.count += action.payload.count;
+        } else state.value = [...state.value, action.payload];
+      }
     },
     plusOne: (state: {value: Product[]}, action: {payload: number}) => {
-      // @ts-ignore
-      findByIdNumber(state, action).count += 1;
+      const thisProduct = state.value.find((n) => n.id === action.payload);
+      if (thisProduct) {
+        thisProduct.count += 1;
+      }
     },
     minusOne: (state: {value: Product[]}, action: {payload: number}) => {
-      // @ts-ignore
-      if (findByIdNumber(state, action).count === 1) {
-        // @ts-ignore
-        state.value.splice(state.value.indexOf(findByIdNumber(state, action)), 1);
-      } else { // @ts-ignore
-        findByIdNumber(state, action).count -= 1;
+      const thisProduct = state.value.find((n) => n.id === action.payload);
+      if (thisProduct) {
+        if (thisProduct.count === 1) {
+          state.value.splice(state.value.indexOf(thisProduct), 1);
+        } else {
+          thisProduct.count -= 1;
+        }
       }
     },
   },
